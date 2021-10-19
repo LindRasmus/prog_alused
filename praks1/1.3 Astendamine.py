1d37b23e432fc8e48a67c09afa29e6892153d8a0
Rasmus Lind <rasmus.lind@khk.ee>
2021-10-19 09:56:12 +0300

    1.3 Astendamine

---
+++ praks1/1.3 Astendamine.py
@@ -0,0 +1,5 @@
+astme_alus = int(input("Sisestage astme alus: "))
+astendaja = int(input("Sisestage astendaja: "))
+vastus = astme_alus ** astendaja
+vastus_1 =("Teie vastus on: ")
+print(vastus_1 + str(vastus))
