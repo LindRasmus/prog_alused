ac09c1f8e944f7ee53d200de16b0c615d4ebe5fb
Rasmus Lind <rasmus.lind@khk.ee>
2021-10-19 09:55:59 +0300

    1.2 President

--- 1.3 Astendamine.py
+++
@@ -1,5 +0,0 @@
-astme_alus = int(input("Sisestage astme alus: "))
-astendaja = int(input("Sisestage astendaja: "))
-vastus = astme_alus ** astendaja
-vastus_1 =("Teie vastus on: ")
-print(vastus_1 + str(vastus))
